fa2f3a461d421fb7ba7627dbd7ae800148bf5887
dadhi-kuriyal <dadhikuriyal@virtualemployee.com>
2023-01-13 11:57:06 +0530

    Added CK editor on the coi detail page

--- public/js/coidetails.js
+++ public/js/coidetails.js
@@ -1,198 +1,210 @@
-$(function() {
-            $(".modalbox").fancybox({'openEffect': 'none',fitToView: true});
-			
-            $( "#tabs" ).tabs();
-			$( "#tabs" ).show();
-            $( "#date_cp_in" ).datepicker({
-             dateFormat:'mm/dd/yy',
-             changeMonth: true,
-             changeYear: true
-             });
-             $( "#date_cp_out" ).datepicker({
-             dateFormat:'mm/dd/yy',
-             changeMonth: true,
-             changeYear: true
-             });
-          });
+$(function () {
+	$(".modalbox").fancybox({ 'openEffect': 'none', fitToView: true });
+
+	$("#tabs").tabs();
+	$("#tabs").show();
+	$("#date_cp_in").datepicker({
+		dateFormat: 'mm/dd/yy',
+		changeMonth: true,
+		changeYear: true
+	});
+	$("#date_cp_out").datepicker({
+		dateFormat: 'mm/dd/yy',
+		changeMonth: true,
+		changeYear: true
+	});
+});
 
 
-function checkNewcoidetails() 
-{  
-    parent.CheckForSessionpop(baseUrl);
-    $('#addnewcoidetails').attr('disabled',true);
-	var certificate_holder = $("#certificate_holder").val();
-	var certificate_description = $("#certificate_description").val();
-	var send_certificate_to = $("#send_certificate_to").val();	
-	
-	var submit_flag = true;
-	if(certificate_holder==''){
-			$('#holder_name_error').html("Please Enter Certificate Holder Information");
-			submit_flag = false;
+function checkNewcoidetails() {
+	parent.CheckForSessionpop(baseUrl);
+	$('#addnewcoidetails').attr('disabled', true);
+	var certificate_holder = CKEDITOR.instances.certificate_holder.getData();
+	var certificate_description = CKEDITOR.instances.certificate_description.getData();
+	var send_certificate_to = CKEDITOR.instances.send_certificate_to.getData();
+	var isError = false;
+
+	if (certificate_holder.length == 0) {
+		$("#holder_name_error").html("Please Enter Certificate Holder Information");
+		isError = true;
+	} else {
+		$("#holder_name_error").html("");
 	}
-	if(certificate_description==''){
-			$('#description_name_error').html("Please Enter Description of Special Terms");
-			submit_flag = false;
+	if (certificate_description.length == 0) {
+		$("#description_error").html("Please Enter Description of Special Terms");
+		isError = true;
+	} else {
+		$("#holder_nadescription_errorme_error").html("");
+	}
+	if (send_certificate_to.length == 0) {
+		$("#send_to_error").html("Please Enter Send Certificate To");
+		isError = true;
+	} else {
+		$("#send_to_error").html("");
 	}
 
-	if(send_certificate_to==''){
-		$('#send_certificate_name_add_error').html("Please Enter Send Certificate To");
-		submit_flag = false;
-}
-	
-	if(!submit_flag) {
-		$('#addnewcoidetails').attr('disabled',false);
-	}
-	if(submit_flag) {
+	if (!isError) {
 		$('.loader').show();
 		$.ajax({
-				url         : baseUrl+"coi/createnewcoidetails",
-				type        : "post",
-				datatype    : 'json',
-				data        : {coi_au_details_holder:certificate_holder,coi_au_details_specialterms:certificate_description,coi_au_details_send_certificate_to:send_certificate_to},
-				success     : function( data ) {
+			url: baseUrl + "coi/createnewcoidetails",
+			type: "post",
+			datatype: 'json',
+			data: { coi_au_details_holder: certificate_holder, coi_au_details_specialterms: certificate_description, coi_au_details_send_certificate_to: send_certificate_to },
+			success: function (data) {
 				var content = $.parseJSON(data);
-                    if(content.status=='success'){
-						$('.loader').hide();
-						//parent.jQuery.fancybox.close();
+				if (content.status == 'success') {
+					$('.loader').hide();
+					//parent.jQuery.fancybox.close();
+					$('#success_msg').html('COI-Detail added successfully!');
+					//$("#success_msg").focus();
+					$(window).scrollTop($('#success_msg').position().top);
+					setTimeout(function () {
 						parent.location.reload();
-						
-					}else{
-						alert('Error occurred');
-						location.reload();
-					}
-				},
-				error       : function(){
-					alert('There was an error');
-				}
+					}, 2000);
 
-			 });
-	
+
+				} else {
+					alert('Error occurred');
+					location.reload();
+				}
+			},
+			error: function () {
+				alert('There was an error');
+			}
+
+		});
+
 	} else {
-	    return false;
+		return false;
 	}
 }
 
-function updateCoidetails() 
-{
- $('#updatenewcoidetails').attr('disabled',true);
-    var cid = $("#cid").val();
-    var bid = $("#bid").val();
-    var uniquecc = $("#uniquecc").val();
-	var certificate_holder = $("#certificate_holder").val();
-	var certificate_description = $("#certificate_description").val();
-	var send_certificate_to = $("#send_certificate_to").val();
+function updateCoidetails() {
+	$('#updatenewcoidetails').attr('disabled', true);
+	var cid = $("#cid").val();
+	var bid = $("#bid").val();
+	var uniquecc = $("#uniquecc").val();
+	var certificate_holder = CKEDITOR.instances.certificate_holder.getData(); //$("#email_content").val(); //tinymce.get('content').getContent();
+	var certificate_description = CKEDITOR.instances.certificate_description.getData(); //$("#email_content").val(); //tinymce.get('content').getContent();
+	var send_certificate_to = CKEDITOR.instances.send_certificate_to.getData(); //$("#email_content").val(); //tinymce.get('content').getContent();
+	var isError = false;
+
 	var submit_flag = true;
-	if(certificate_holder==''){
-			$('#holder_name_error').html("Please Enter Certificate Holder Information");
-			submit_flag = false;
+	if (certificate_holder.length == 0) {
+		$("#holder_name_error").html("Please enter certificate holder");
+		isError = true;
+	} else {
+		$("#holder_name_error").html("");
 	}
-	
-	if(certificate_description==''){
-			$('#description_name_error').html("Please Enter Description of Special Terms");
-			submit_flag = false;
+	if (certificate_description.length == 0) {
+		$("#description_error").html("Please enter description of operations/locations/vehicles/special term");
+		isError = true;
+	} else {
+		$("#holder_nadescription_errorme_error").html("");
 	}
-	
-		if(send_certificate_to==''){
-			$('#send_certificate_name_error').html("Please Enter Send Certificate To");
-			submit_flag = false;
+	if (send_certificate_to.length == 0) {
+		$("#send_to_error").html("Please enter send certificate to");
+		isError = true;
+	} else {
+		$("#send_to_error").html("");
 	}
-	
-	if(!submit_flag) {
-		$('#updatenewcoidetails').attr('disabled',false);
-	}
-	if(submit_flag) {
+
+	if (!isError) {
 		$('.loader').show();
 		$.ajax({
-				url         : baseUrl+"coi/updatecoidetailstest",
-				type        : "post",
-				datatype    : 'json',
-				data        : {coi_au_details_ID:cid,Building_ID:bid,uniqueCostCenter:uniquecc,coi_au_details_holder:certificate_holder,coi_au_details_specialterms:certificate_description,coi_au_details_send_certificate_to:send_certificate_to},
-				success     : function( data ) {
+			url: baseUrl + "coi/updatecoidetailstest",
+			type: "post",
+			datatype: 'json',
+			data: { coi_au_details_ID: cid, Building_ID: bid, uniqueCostCenter: uniquecc, coi_au_details_holder: certificate_holder, coi_au_details_specialterms: certificate_description, coi_au_details_send_certificate_to: send_certificate_to },
+			success: function (data) {
 				var content = $.parseJSON(data);
-                    if(content.status=='success'){
-						$('.loader').hide();
-						//parent.jQuery.fancybox.close();
+				if (content.status == 'success') {
+					$('#success_msg').html('COI-Detail edited successfully!');
+					//$("#success_msg").focus();
+					$(window).scrollTop($('#success_msg').position().top);
+					setTimeout(function () {
 						parent.location.reload();
-						
-					}else{
-						alert('Error occurred');
-						location.reload();
-					}
-				},
-				error       : function(){
-					alert('There was an error');
-				}
+					}, 2000);
+					$('.loader').hide();
 
-			 });
-	
+
+				} else {
+					alert('Error occurred');
+					location.reload();
+				}
+			},
+			error: function () {
+				alert('There was an error');
+			}
+
+		});
+
 	} else {
-	    return false;
+		return false;
 	}
-		
-		
+
+
 }
 
-function cancelReport() 
-{
+function cancelReport() {
 	parent.jQuery.fancybox.close();
 }
 
-function showEditCoidetails(id)
-{
-	
+function showEditCoidetails(id) {
+
 	$('.loader').show();
-	if(id!='') {
+	if (id != '') {
 		$.ajax({
-					url         : baseUrl+"coi/editcoidetails",
-					type        : "post",
-					data        : {id:id},
-					success     : function( data ) {
-							$('.loader').hide();
-							$('#show_Edit_Report_div').html(data);
-							$('#show_Edit_Report_div_href').trigger('click');
-							
-							//reloadPage();
-					},
-					error       : function(){
-						alert('There was an error');
-					}
+			url: baseUrl + "coi/editcoidetails",
+			type: "post",
+			data: { id: id },
+			success: function (data) {
+				$('.loader').hide();
+				$('#show_Edit_Report_div').html(data);
+				$('#show_Edit_Report_div_href').trigger('click');
+
+				//reloadPage();
+			},
+			error: function () {
+				alert('There was an error');
+			}
 
 		});
 	}
 }
 
-function deleteCoiDetails(cid) 
-{
-	var check_delete='YES';
-    var return_value = jPrompt("For Deleting Coi Details, Enter Yes in Capital letters.", '', 'Vision Work Orders', function(return_value) { 
-        if(return_value!=null){
-                if(check_delete === return_value){
-                $('.loader').show();
-	if(cid!='') {
-		$.ajax({
-					url         : baseUrl+"coi/deletecoidetails",
-					type        : "post",
-					datatype    : 'json',
-					data        : {cid:cid},
-					success     : function( data ) {
-					var content = $.parseJSON(data);
-						if(content.status=='success'){
-							$('.loader').hide();
-							location.reload();
-						}else{
-							alert('Error occurred');
-							location.reload();
+function deleteCoiDetails(cid) {
+	var check_delete = 'YES';
+	var return_value = jPrompt("For Deleting Coi Details, Enter Yes in Capital letters.", '', 'Vision Work Orders', function (return_value) {
+		if (return_value != null) {
+			if (check_delete === return_value) {
+				$('.loader').show();
+				if (cid != '') {
+					$.ajax({
+						url: baseUrl + "coi/deletecoidetails",
+						type: "post",
+						datatype: 'json',
+						data: { cid: cid },
+						success: function (data) {
+							var content = $.parseJSON(data);
+							if (content.status == 'success') {
+								$('.loader').hide();
+								location.reload();
+							} else {
+								alert('Error occurred');
+								location.reload();
+							}
+						},
+						error: function () {
+							alert('There was an error');
 						}
-					},
-					error       : function(){
-						alert('There was an error');
-					}
 
-		});
-	}
-	 } else{
-                $('#cw_error').html('You have entered wrong word.');
-            }
-        } } );
+					});
+				}
+			} else {
+				$('#cw_error').html('You have entered wrong word.');
+			}
+		}
+	});
 }
 
